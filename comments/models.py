fdaa93dd9610b1e376f878ee027694f21a3c5186
araldwin <araldwin@gmail.com>
2023-08-08 15:23:38 +0000

    create comments app model and models, add CommentList and CommentDetail views

---
+++ comments/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from pins.models import Pin
+
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Pin
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    pin = models.ForeignKey(Pin, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
